26c2a01e7b64a9b5870c55c09ec0db67fc299656
Fernando Deluret <fernandodeluret@gmail.com>
2022-05-04 12:24:55 -0300

    fer ruining code

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -2,6 +2,7 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 const TWEET_PDA:&str = "TWEET_ACC";
+const SEED1: u64 = 100;
 #[program]
 pub mod solana_twitter {
     use super::*;
@@ -9,18 +10,20 @@ pub mod solana_twitter {
     pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String, twitter_id: u64) -> Result<()> {
         let tweet = &mut ctx.accounts.tweet;
         let author = &ctx.accounts.author;
-        let clock = Clock::get().unwrap();
-        if topic.chars().count() > 50 {
-            return Err(ErrorCode::TopicTooLong.into());
-        }
-        if content.chars().count() > 200 {
-            return Err(ErrorCode::ContentTooLong.into());
-        }
-        tweet.author = *author.key;
-        tweet.timestamp = clock.unix_timestamp;
-        tweet.topic = topic;
-        tweet.content = content;
-        tweet.twitter_id = twitter_id;
+        // let clock = Clock::get().unwrap();
+        // if topic.chars().count() > 50 {
+        //     return Err(ErrorCode::TopicTooLong.into());
+        // }
+        // if content.chars().count() > 200 {
+        //     return Err(ErrorCode::ContentTooLong.into());
+        // }
+        // tweet.author = author.key();
+        // tweet.timestamp = clock.unix_timestamp;
+        // tweet.topic = topic;
+        // tweet.content = content;
+        // tweet.twitter_id = twitter_id;
+        msg!("{:?}", tweet.key());
+        return Err(ErrorCode::TopicTooLong.into());
         
         Ok(())
     }
@@ -29,15 +32,20 @@ pub mod solana_twitter {
 #[derive(Accounts)]
 #[instruction(twitter_id: u64)]
 pub struct SendTweet<'info> {
-    #[account(init, payer = author, space = Tweet::LEN, seeds=[
-        &twitter_id.to_be_bytes(),
-        TWEET_PDA.as_bytes(),
-        author.key().as_ref(),
-    ],bump)]
-    pub tweet: Account<'info,Tweet>,
+    #[account(
+        init, 
+        payer = author, 
+        space = Tweet::LEN, 
+        seeds=[
+            twitter_id.to_be_bytes().as_ref(),
+            TWEET_PDA.as_bytes(),
+            author.key().as_ref(),
+        ],
+        bump)]
+    pub tweet: Account<'info, Tweet>,
     #[account(mut)]
     pub author: Signer<'info>,
-    pub system_program: Program<'info,System>
+    pub system_program: Program<'info, System>
     
 
 }
@@ -53,7 +61,7 @@ pub struct Tweet {
 }
 
 impl Tweet {
-    pub const LEN: usize = 8 + 32 + 8 + 204 + 4 + 280*4;
+    pub const LEN: usize = 8 + 32 + 8 + 204 + 4 + 280*4 + 80;
 }
 
 #[error_code]
--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -15,21 +15,39 @@ describe("solana-twitter", () => {
   
   
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
-  const author = anchor.web3.Keypair.generate();
-  const adminAccount = new anchor.Wallet(author); 
-  const adminProvider = new anchor.AnchorProvider(program.provider.connection, adminAccount,{
-    commitment: "recent",
-    preflightCommitment: "recent",
-    skipPreflight: true
-  });
+  // const author = anchor.web3.Keypair.generate();
+  // const adminAccount = new anchor.Wallet(author); 
+  // const adminProvider = new anchor.AnchorProvider(program.provider.connection, adminAccount,{
+  //   commitment: "recent",
+  //   preflightCommitment: "recent",
+  //   skipPreflight: true
+  // });
   it("Is initialized!", async () => {
-    // const tweet_acc = anchor.web3.Keypair.generate();
+
+
+
+    let twID: anchor.BN = new anchor.BN(60)
+    let [pub1] = await anchor.web3.PublicKey.findProgramAddress(
+      // [u64ToSeed(twID), Buffer.from("TWEET_ACC"), provider.wallet.publicKey.toBuffer()], 
+      [twID.toBuffer("be", 8), Buffer.from("TWEET_ACC"), provider.wallet.publicKey.toBuffer()], 
+      program.programId);
+    
+    console.log("pub1", pub1.toBase58())
 
     // const provider = anchor.getProvider;
-    const tx = await program.methods.sendTweet('Toys','Roblox is the best east or west', new anchor.BN(60)).accounts({
-      // tweet: tweet_acc.publicKey,
+    // const tx = await program.rpc.sendTweet('Toys','Roblox is the best east or west', twID, {
+    //   accounts:{
+    //     tweet: pub1,
+    //     author: provider.wallet.publicKey,
+    //     systemProgram: anchor.web3.SystemProgram.programId
+    //   }
+    // })
+    // const tx = await program.methods.sendTweet('Toys','Roblox is the best east or west', twID).accounts({
+    const tx = await program.methods.sendTweet('Toys','Roblox is the best east or west', twID).accounts({
+      // tweet: new anchor.web3.PublicKey('G6jPLm4DPMefgyXqYqKXKFHnYjWoKiH33Hc6Zkn8DdwL'),
+      tweet: pub1,
       author: provider.wallet.publicKey,
-      
+      systemProgram: anchor.web3.SystemProgram.programId
     }).rpc();
     // const tx = new anchor.web3.Transaction().add(ix);
     // const txID = await provider.(tx,[provider.wallet]);
@@ -37,3 +55,8 @@ describe("solana-twitter", () => {
     console.log("Your transaction signature", tx);
   });
 });
+
+
+export function u64ToSeed(num: anchor.BN) {
+  return num.toBuffer('le', 8);
+}
